4fbe38821fdb65aa8586618b4dcbf96b55932a91
TheFutureCode2021 <faizreplayz@gmail.com>
2022-02-03 13:20:35 +0600

    'init'

--- modules/moderation/cog.py
+++ modules/moderation/cog.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord 
 from discord.ext import commands 
 
@@ -11,6 +12,18 @@ class Moderation(commands.Cog(name='Moderation',description='Moderation commands
         print(f'{self.__cog_name__} is Ready\n----------------------')
 
     @commands.command()
-    async def kick(self, member:commands.MemberConverter, *,reason:str=None):
+    async def kick(self, ctx:commands.Context,member:commands.MemberConverter, *,reason:str=None):
         """Kick a member from the server"""
-        if member is discord.utils.get(self.bot.get)
+        if member is None:
+            await ctx.reply(f'**{member}** is not found!\nPlease specify a member to kick')
+        else:
+            msg = await ctx.send(f'`{member}` has been found and is being kicked')
+            await asyncio.sleep(2)
+            await member.kick(reason=reason)
+            await msg.edit(f'`{member}` has been kicked for reason {reason}')
+            await member.send(f'You have been kicked from the server {ctx.guild.name} for reason: {reason}')
+
+
+
+def setup(bot:commands.Bot):
+    bot.add_cog(Moderation(bot))
